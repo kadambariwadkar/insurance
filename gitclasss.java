e97540212b931d885d8f7d0d24944efa1d2f7543
kadambari wadkar <wadkarkadambari@gamil.com>
2022-12-17 17:57:37 +0530

    pune

---
+++ gitclasss.java
@@ -0,0 +1,19 @@
+package git_practice;
+
+public class gitclasss {
+
+	public static void main(String[] args) 
+	{
+		for(int i=2;i<=20;i=i+2)
+		{
+			System.out.println(i);
+		}
+		System.out.println("=====================");
+	
+	for (int j=20;j>=2;j=j-2)
+	{
+		System.out.println(j);
+	}
+	}
+
+}
